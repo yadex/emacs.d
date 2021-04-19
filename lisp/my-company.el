1c0f8afbb11fbf527a523b2a1160d91890184e7c
yadex <wxn834577101@gmail.com>
2021-04-19 09:22:36 +0800

    update

--- lisp/my-company.el
+++ lisp/my-company.el
@@ -14,8 +14,17 @@
            company-yasnippet
            )
           (company-abbrev company-dabbrev))))
+;(setq company-format-margin-function #'company-vscode-light-icons-margin)
+;(setq company-format-margin-function #'company-text-icons-margin)
 
 (add-hook 'emacs-lisp-mode-hook (lambda () (set (make-local-variable 'company-backends) '(company-elisp))))
+
+;(use-package company-box
+;  :ensure t
+;  :hook (company-mode . company-box-mode)
+;
+;  )
+
 
 ;;
 ;; change C-n C-p
@@ -25,6 +34,7 @@
   (define-key company-active-map (kbd "C-p") #'company-select-previous)
   (define-key company-active-map (kbd "M-n") nil)
   (define-key company-active-map (kbd "M-p") nil))
+
 
 
 (provide 'my-company)
